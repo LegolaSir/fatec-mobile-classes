98d9ea9eddd5cb35337883ace5ad038585f72ed6
BRUNO GONCALVES DE ALENCAR <0040962013023@americana.fatec.br>
2022-08-27 10:49:30 -0300

    implementing mouse control

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -32,15 +32,46 @@ public class PlayerBehaviour : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
+        MoveForwards();
+        //MoveByKeyboard();
+        MoveByMouse();
+    }
+
+    public void MoveByKeyboard()
+    {
         var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed * Time.fixedDeltaTime;
 
         // Check if we're moving to the side 
-        rb.AddForce(0, 0, rollSpeed);
-        if (rb.velocity.z >= rollSpeed) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rollSpeed);
-
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             this.transform.position += new Vector3(horizontalSpeed, 0, 0);
         }
+    }
+
+    public void MoveByMouse()
+    {
+        float horizontalSpeed = dodgeSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            // Converting mouse click or touch into a viewport point, in order to simplify interaction towards game world space
+            Vector3 worldPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            // Proper movement in X axis, related to 'worldPos'
+            int xMove;
+
+            // Setting player direction towards X axis according to 'worldPos' value
+            _ = (worldPos.x < 0.5f) ? xMove = -1 : xMove = 1;
+
+            // X value to be implemented into new player location
+            horizontalSpeed *= xMove;
+            this.transform.position += new Vector3(horizontalSpeed, 0, 0);
+        }
+    }
+
+    public void MoveForwards()
+    {
+        rb.AddForce(0, 0, rollSpeed);
+        if (rb.velocity.z >= rollSpeed) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rollSpeed);
     }
 }
