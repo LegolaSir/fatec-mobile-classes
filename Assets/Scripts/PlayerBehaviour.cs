52a9331a2936518bd47b1c21ed2bdd43f14658e0
BRUNO GONCALVES DE ALENCAR <0040962013023@americana.fatec.br>
2022-08-27 10:07:47 -0300

    first commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary> 
+/// Responsible for moving the player automatically and 
+/// reciving input. 
+/// </summary> 
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    /// <summary> 
+    /// A reference to the Rigidbody component 
+    /// </summary> 
+    private Rigidbody rb;
+
+    [Tooltip("How fast the ball moves left/right")]
+    public float dodgeSpeed = 5;
+
+    [Tooltip("How fast the ball moves forwards automatically")]
+    [Range(0, 10)]
+    public float rollSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get access to our Rigidbody component 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// FixedUpdate is called at a fixed framerate and is a prime place to put
+    /// Anything based on time.
+    /// </summary>
+    void FixedUpdate()
+    {
+        var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed * Time.fixedDeltaTime;
+
+        // Check if we're moving to the side 
+        rb.AddForce(0, 0, rollSpeed);
+        if (rb.velocity.z >= rollSpeed) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rollSpeed);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += new Vector3(horizontalSpeed, 0, 0);
+        }
+    }
+}
